d44b48f922a0319c4303e73e1f0326420463f3a7
am25112-ZHANG-LEYI <am25112@shibaura-it.ac.jp>
2025-12-09 20:31:24 +0900

    ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+int main() 
+{
+ int a[5], b[5];
+ int i, j=0;
+ for(i=0; i<5; i++)
+ {
+  scanf("%d", &a[i]);
+ }
+
+  for(i = 0; i < 5; i++)
+    {
+        b[i] = a[i];
+    }
+    for(i = 0; i < 4; i++)
+    {
+        for(j = i + 1; j < 5; j++)
+        {
+            if(b[i] > b[j])
+            {
+                int temp = b[i];
+                b[i] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+    
+    printf("sorted array:\n");
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
 
