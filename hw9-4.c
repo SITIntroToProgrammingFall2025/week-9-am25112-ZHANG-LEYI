2679b9efa9c3868d98c1ffc53b56ca9a150a35b5
am25112-ZHANG-LEYI <am25112@shibaura-it.ac.jp>
2025-12-09 20:40:20 +0900

    hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int rgb[3];
-    char hexDigits[] = "0123456789ABCDEF";
-    char hexCode[7];
+ int r, g, b;
+ scanf("%d", &r);
+ scanf("%d", &g);
+ scanf("%d", &b);
 
-    scanf("%d", &rgb[0]);
-    scanf("%d", &rgb[1]);
-    scanf("%d", &rgb[2]);
+ printf("The hex code is #%02X%02X%02X\n", r, g, b);
 
-    for (int i = 0; i < 3; i++) {
-        int first = rgb[i] / 16;
-        int second = rgb[i] % 16;
-        hexCode[i*2] = hexDigits[first];
-        hexCode[i*2 + 1] = hexDigits[second];
-    }
-
-    hexCode[6] = '\0';
-
-    printf("The hex code is #%s", hexCode);
-
-    return 0;
+ return 0;
 }
+
 
