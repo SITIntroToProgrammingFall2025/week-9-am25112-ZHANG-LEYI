de99fff26f1988e579708e939a77181034af9078
am25112-ZHANG-LEYI <am25112@shibaura-it.ac.jp>
2025-12-09 18:45:40 +0900

    hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+    int m[2][2];
+    double det;
+
+    scanf("%d", &m[0][0]);
+    scanf("%d", &m[0][1]);
+    scanf("%d", &m[1][0]);
+    scanf("%d", &m[1][1]);
+
+    det = (double)m[0][0] * m[1][1] - (double)m[0][1] * m[1][0];
+
+    printf("You entered\n");
+    printf("%d %d\n", m[0][0], m[0][1]);
+    printf("%d %d\n", m[1][0], m[1][1]);
+    printf("Determinant is %f\n", det);
+
+    return 0;
+}
+
 
