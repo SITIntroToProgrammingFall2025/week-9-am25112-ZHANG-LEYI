13c859299791374b84211d0e5c2b10c0453a1031
am25112-ZHANG-LEYI <am25112@shibaura-it.ac.jp>
2025-12-09 18:59:26 +0900

    hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-int main(void) {
-    int a[2][2];   
-    double det;
+int main() {
+    double a[2][2];
+    int i, j;
 
-    scanf("%d", &a[0][0]);
-    scanf("%d", &a[0][1]);
-    scanf("%d", &a[1][0]);
-    scanf("%d", &a[1][1]);
+    printf("Enter 2 x 2 matrix\n");
 
-    det = (double)a[0][0] * a[1][1] - (double)a[0][1] * a[1][0];
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 2; j++){
+            scanf("%lf", &a[i][j]);
+        }
+    }
 
     printf("You entered\n");
-    printf("%d %d\n", a[0][0], a[0][1]);
-    printf("%d %d\n", a[1][0], a[1][1]);
+    printf("%.0f %.0f\n", a[0][0], a[0][1]);
+    printf("%.0f %.0f\n", a[1][0], a[1][1]);
+
+    double det = a[0][0] * a[1][1] - a[0][1] * a[1][0];
     printf("Determinant is %f\n", det);
 
     return 0;
 }
+
 
 
 
