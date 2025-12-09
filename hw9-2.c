212825f75fcdcacc5c082cbac8c71e252e50933e
am25112-ZHANG-LEYI <am25112@shibaura-it.ac.jp>
2025-12-09 19:00:43 +0900

    hw9-2.c
    Fix the input and output loops for the 3x3 array.

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3];
+    int b[3][3];
+    int i, j;
+
+    printf("Enter 3 x 3 array\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < 3; i++) {
+        b[i][0] = a[i][2];
+        b[i][1] = a[i][1];
+        b[i][2] = a[i][0];
+    }
+
+    printf("You entered\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Output\n");
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
